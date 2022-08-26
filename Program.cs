7b25f5ad39d94b3b12ea530b36e70e7dc6c31c7e
Milan Karki <milankarki723@gmail.com>
2022-08-26 07:17:31 +0545

    Age Requirement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿    using System;
-   SayHello();
-   void SayHello(){
-    Console.WriteLine("Hello World!");
+﻿using System;
+string name;
+int age;
+Console.Write("Enter your Name");
+name= Console.ReadLine();
+Console.Write("Enter your age ");
+    age = int.Parse(Console.ReadLine());
+   if (age < 18){
+    Console.WriteLine("Under Age");
+   }else{
+    Console.WriteLine("qualified Age");
    }
