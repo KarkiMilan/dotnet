e69df991e8c4047a3598d7457faa05d1bbf9951c
Milan Karki <milankarki723@gmail.com>
2022-08-26 06:52:54 +0545

    Hello world

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿    using System;
-    Console.WriteLine(FeetToInches(60));
-    Console.WriteLine(FeetToInches(100));
-
-    int FeetToInches(int feet)
-  {
-    int inches = feet * 12;
-    return inches;
-  }
+   SayHello();
+   void SayHello(){
+    Console.WriteLine("Hello World!");
+   }
