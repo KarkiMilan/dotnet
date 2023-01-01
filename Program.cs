b8bdca2f60a22185247ed46ecf4ad3d057d8af94
karkimilan <Milankarki723@gmail.com>
2023-01-01 08:52:06 +0545

    Making Proper generic class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿        bool equal = Calculator.AreEqual(4,4);
+﻿        bool equal = Calculator <int>.AreEqual(4,4);
         Console.WriteLine(equal);
-        bool equal1 = Calculator.AreEqual("Nepal","Switerland");
+        bool equal1 = Calculator<string>.AreEqual("Nepal","Switerland");
         Console.WriteLine(equal1);
-public class Calculator
+public class Calculator <T>
 {
     
-    public static bool AreEqual <T> (T value1, T value2)
+    public static bool AreEqual (T value1, T value2)
     {
         return value1.Equals(value2);
 
