a9aa29b88c4cca68f8a4fdf67d43587db8b739dc
karkimilan <Milankarki723@gmail.com>
2023-01-01 08:31:18 +0545

    Generics Example

--- Program.cs
+++ Program.cs
@@ -1,52 +1,13 @@
-﻿public readonly struct Fraction
+﻿        bool equal = Calculator.AreEqual(4,4);
+        Console.WriteLine(equal);
+        //bool equal = calculator.AreEqual("Nepal","Switerland");
+        //Console.WriteLine(equal);
+public class Calculator
 {
-    private readonly int num;
-    private readonly int den;
-
-    public Fraction(int numerator, int denominator)
+    
+    public static bool AreEqual (int value1, int value2)
     {
-        if (denominator == 0)
-        {
-            throw new ArgumentException("Denominator cannot be zero.", nameof(denominator));
-        }
-        num = numerator;
-        den = denominator;
-    }
+        return value1.Equals(value2);
 
-    public static Fraction operator +(Fraction a) => a;
-    public static Fraction operator -(Fraction a) => new Fraction(-a.num, a.den);
-
-    public static Fraction operator +(Fraction a, Fraction b)
-        => new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
-
-    public static Fraction operator -(Fraction a, Fraction b)
-        => a + (-b);
-
-    public static Fraction operator *(Fraction a, Fraction b)
-        => new Fraction(a.num * b.num, a.den * b.den);
-
-    public static Fraction operator /(Fraction a, Fraction b)
-    {
-        if (b.num == 0)
-        {
-            throw new DivideByZeroException();
-        }
-        return new Fraction(a.num * b.den, a.den * b.num);
-    }
-
-    public override string ToString() => $"{num} / {den}";
-}
-
-public static class OperatorOverloading
-{
-    public static void Main()
-    {
-        var a = new Fraction(5, 4);
-        var b = new Fraction(1, 2);
-        Console.WriteLine(-a);   // output: -5 / 4
-        Console.WriteLine(a + b);  // output: 14 / 8
-        Console.WriteLine(a - b);  // output: 6 / 8
-        Console.WriteLine(a * b);  // output: 5 / 8
-        Console.WriteLine(a / b);  // output: 10 / 4
     }
 }
