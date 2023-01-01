eab2a0afe79f63cdc603c955f4beb4077843739d
karkimilan <Milankarki723@gmail.com>
2023-01-01 08:45:57 +0545

    Generics Modification of Methods

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 ﻿        bool equal = Calculator.AreEqual(4,4);
         Console.WriteLine(equal);
-        //bool equal = calculator.AreEqual("Nepal","Switerland");
-        //Console.WriteLine(equal);
+        bool equal1 = Calculator.AreEqual("Nepal","Switerland");
+        Console.WriteLine(equal1);
 public class Calculator
 {
     
-    public static bool AreEqual (int value1, int value2)
+    public static bool AreEqual <T> (T value1, T value2)
     {
         return value1.Equals(value2);
 
